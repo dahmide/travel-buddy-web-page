9b80991a451aac869e0993b4c758a4ab760cbdf7
dahmide <blarrklad@gmail.com>
2023-12-07 00:56:58 +0100

    Further commit: Added main.css file

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import {
     RouterProvider,
     Route
 } from "react-router-dom";
+import Login from "./views/Login";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/">
-            
+            <Route path="login" element={<Login />} />
         </Route>
     )
 );
